f9f8a668d142068a14c1b7d5e12d941f4f5264d8
Greg Hale <imalsogreg@gmail.com>
2013-07-04 23:35:07 -0400

    playing w/ StateT over IO but I think that may be wrong.  Seems like the fact that my state is burried in the faucet type makes it hard to find a function a -> StateT...

--- testFaucet.hs
+++ testFaucet.hs
@@ -7,7 +7,8 @@ import Control.Monad.State
 
 data FaucetCommand = FaucetStart | FaucetStop | FaucetDestroy (MVar ())
 
-data FaucetState s = FaucetState { faucetFun :: StateT s IO ()
+data FaucetState s = FaucetState { faucetFun          :: StateT s IO ()
+                                 , faucetRunning      :: Bool
                                  , faucetInitTime     :: UTCTime
                                  , faucetLastRunStart :: Maybe UTCTime
                                  , faucetLastRunEnd   :: Maybe UTCTime
@@ -15,15 +16,23 @@ data FaucetState s = FaucetState { faucetFun :: StateT s IO ()
                    
 data Faucet s = Faucet (MVar (FaucetState s)) (MVar FaucetCommand)
 
-initFaucet :: IO (Faucet s)
-initFaucet = do
+initFaucet :: StateT s IO () -> IO (Faucet s)
+initFaucet sFun= do
   cmdM <- newEmptyMVar
   startTime <- getCurrentTime
-  stM <- newMVar $  FaucetState  sFun startTime Nothing  Nothing
+  stM <- newMVar $  FaucetState  sFun False startTime Nothing  Nothing
   return $ Faucet stM cmdM
-    where
-      sFun :: s -> IO (a,s)
-      sFun = 
+
+runFaucet :: Faucet s -> IO ()
+runFaucet (Faucet stateMVar cmdMVar) = do
+  (FaucetState fFun fRun _ _ _) <- takeMVar stateMVar
+  case fRun of
+    False -> return ()
+    True -> do
+      tStart <- getCurrentTime
+      
+             
+  
 
 
 main :: IO ()
